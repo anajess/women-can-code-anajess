92fecdbb345c4f43cb315dfc7146235d0e10f984
Ana Jessica <ajmoreira98@gmail.com>
2021-11-11 15:58:07 -0300

    exercicio aula 3

---
+++ aula3/exercicio1.js
@@ -0,0 +1,5 @@
+let modelo = "sandero";
+let cor = "prata";
+let ano = "2015";
+let novo = true;
+console.log(modelo, cor, ano, novo);
---
+++ aula3/exercicio2.js
@@ -0,0 +1,5 @@
+let meuNome;
+console.log(meuNome);
+//dá como resultado vazio, porque a variavem não tem informação
+meuNome = "Ana Jessica"
+console.log(meuNome);
