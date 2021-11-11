a61bcefc70bfb2e61381d2372751450584d26388
Ana Jessica <ajmoreira98@gmail.com>
2021-11-11 16:57:23 -0300

    lista 3 subindo

---
+++ lista3/exercicio1.js
@@ -0,0 +1,6 @@
+var hojeVaiChover = false;
+if (hojeVaiChover == true){
+    console.log ("Leve seu guarda-chuva, hoje vai chover");
+}else{
+    console.log("Fique tranquilo, hoje será um dia se sol");
+}
---
+++ lista3/exercicio2.js
@@ -0,0 +1,38 @@
+var a = 5;
+var a1 = 1;
+if (a > a1){
+    console.log(a, " é maior que ", a1);
+}
+var b =2000000;
+var b1 = 3000000000;
+if (b < b1){
+    console.log( b, "é menor que ", b1);
+}
+var c = "Banana";
+var c1= "Banana";
+if(c==c1){
+    console.log( c, " é igual a ", c1);
+}
+var d = 50;
+var d1 = 50;
+if(d===d1){
+    console.log( d, "é estritamente igual a ", d1);
+}
+
+var e = "Carro";
+var e1 = "bicicleta";
+if (e != e1){
+    console.log (e, " é diferente de ", e1);
+}
+
+var f= 50;
+var f1 = 27;
+if(f>f1){
+    console.log(f, " é maior que ", f1);
+}
+
+var g= 975;
+var g1 = 1200;
+if(g<=g1){
+    console.log(g, " é menor ou igual a ", g1);
+}
